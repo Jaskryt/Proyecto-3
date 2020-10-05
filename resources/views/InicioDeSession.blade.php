19ba42e3bea25932bf7bc06d1543edbc36d7153c
Jaskryt <jurassicandycraft@gmail.com>
2020-10-05 02:15:36 -0400

    Login-1.1

--- resources/views/InicioDeSession.blade.php
+++ resources/views/InicioDeSession.blade.php
@@ -1,7 +1,8 @@
 <form method="POST" action="{{ route('Riniciar') }}">
 	@csrf
-	<input type="text" name="nickname" placeholder="Ingrese su nickname..." value="{{ old('nombre') }}"><br>
-	<input type="password" name="contraseña" placeholder="Ingrese contraseña..."><br>
-	<button>Enviar</button>
+	<center style='padding-top: 300px'>
+		<input type="text" name="nickname" placeholder="Ingrese su nickname..." value="{{ old('nombre') }}"><br>
+		<input type="password" name="contraseña" placeholder="Ingrese contraseña..."><br>
+		<button>Enviar</button>
+	</center>
 </form>
-
